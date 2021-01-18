bb9d90666d191e0c8fa7ebd8016f1fc1e8fe9574
atheists <61435483+atheists@users.noreply.github.com>
2021-01-17 20:17:54 -0600

    Move Javascript into its own file

---
+++ static/main.js
@@ -0,0 +1,50 @@
+function prettyDuration(ms) {
+	const totalSeconds = Math.floor(ms/1000);
+    const h = Math.floor(totalSeconds / 3600);
+    const m = Math.floor((totalSeconds % 3600) / 60);
+    const s = Math.floor(totalSeconds % 60);
+    return (
+		(h < 10 ? "0" : "") + h + ":" +
+		(m < 10 ? "0" : "") + m + ":" +
+		(s < 10 ? "0" : "") + s
+	);
+}
+
+function updateTimerValue() {
+	const timer = document.getElementById("timer");
+	const start = timer.dataset.start;
+	if (!start) {
+		// No timer running
+		timer.innerText = prettyDuration(0);
+		return;
+	}
+	const now = (new Date()).getTime();
+	timer.innerText = prettyDuration(now - start);
+}
+
+function init() {
+	document.getElementById("reset-timer").addEventListener("click", () => {
+		timer.dataset.start = "";
+		updateTimerValue();
+	});
+	document.getElementById("start-timer").addEventListener("click", () => {
+		const timer = document.getElementById("timer");
+		const start = timer.dataset.start;
+		if (start) {
+			// Timer is already running.  Do nothing.
+			return;
+		}
+		timer.dataset.start = (new Date()).getTime();
+	});
+	timer.innerText = prettyDuration(0);
+	setInterval(updateTimerValue, 1000);
+
+	document.getElementById("clear-message").addEventListener("click", () => {
+		document.getElementById("message").value = "";
+	});
+	document.getElementById("send-message").addEventListener("click", () => {
+		const message = document.getElementById("message").value;
+		document.getElementById("message-read").textContent = message;
+	});
+}
+
